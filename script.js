23cc8ddd1b2d5a7e6b89dca67dfe22ad66a3a20c
mresendiz14 <107743452+mresendiz14@users.noreply.github.com>
2024-11-20 04:45:35 +0000

    fixed bugs

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-getElementById('funfactButton').addEventListener('click', function() {
-  const funFact = document.getElementById('funfact');
+
+//JS bug #1 requires document.getElementByID 
+//JS bug #2 the ID funfactButton was not capitalized like in FunFact
+document.getElementById('funFactButton').addEventListener('click', function() {
+  const funFact = document.getElementById('funFact');
   if (funFact.style.display === 'none') {
       funFact.style.display = 'block';
   } else {
